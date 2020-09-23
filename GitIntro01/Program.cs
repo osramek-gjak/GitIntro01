a08ab58f6dfd2a434e85426fe2b8f25932f23358
Ondrej Sramek <o.sramek-gjak@gmail.com>
2020-09-23 15:38:01 +0200

    Pozdrav ma byt Ahoj Svete

--- GitIntro01/Program.cs
+++ GitIntro01/Program.cs
@@ -6,7 +6,7 @@ namespace GitIntro01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ahoj Svete!");
         }
     }
 }
