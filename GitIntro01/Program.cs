767fa62a2c8c78ac936e9d9fb31e35b6172fb995
ucitel <ucitel@gjak.local>
2020-09-23 15:09:52 +0200

    Přidejte soubory projektu.

---
+++ GitIntro01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitIntro01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
